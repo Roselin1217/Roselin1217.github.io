7a95303d74c53235ceb0b0cc86992f4add8224bc
Roselin1217 <12roselin17@gmail.com>
2025-11-24 12:11:59 +0800

    Site updated: 2025-11-24 12:11:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/22/1.LED底层代码/","2025/11/22/0.Keil工程模板搭建指南/","2025/11/22/hello-world/"];function toRandomPost(){
+var posts=["2025/11/22/0.Keil工程模板搭建指南/","2025/11/22/1.LED底层代码/","2025/11/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
